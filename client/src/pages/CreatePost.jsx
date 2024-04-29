9818fc9cb704a5c9283c45d9f6143e3a3c664277
Yana Mishyna <yana.mishina.92@gmail.com>
2024-04-29 18:11:25 -0400

    cteared CreatePost logic and frontend

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,13 +15,20 @@ const CreatePost = () => {
     const [generatingImg, setGeneratingImg] = useState(false); // loading state
     const [loading, setLoading] = useState(false); // loading state
 
+    const generateImg = () => {
+        
+    }
+
     const handleSubmit = () => {
     }
 
     const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value});
     }
 
     const handleSurpriseMe = () => {
+        const randomPrompt = getRandomPrompt(form.promt);
+        setForm({ ...form, promt: randomPrompt });
     }
 
 
@@ -51,6 +58,49 @@ const CreatePost = () => {
                         isSurpriseMe
                         handleSurpriseMe={handleSurpriseMe}
                     />
+                    <div className="relative bg-gray-50 borger border-gray-300 text-gray-900 text-sm rounded-lg focuse:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+                        {form.photo ? (
+                            <img
+                                src={form.photo}
+                                alt={form.promt}
+                                className="w-full h-full object-contain"
+                            />
+                        ) : (
+                            <img
+                                src={preview}
+                                alt="preview"
+                                className="w-9/12 h-9/12 object-contai opacity-40"
+                            />
+                        )}
+
+                        {generatingImg && (
+                            <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgbs(0, 0, 0, 0, 0.5)] rounded-lg">
+                                <Loader />
+                            </div>
+
+
+
+                        )}
+
+                    </div>
+                </div>
+                <div className="mt-5 flex gap-05">
+                    <button
+                        type="button"
+                        onClick={generateImg}
+                        className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    >
+                        {generateImg ? "Generating Image..." : "Generate Image"}
+                    </button>
+                </div>
+                <div className="mt-10">
+                    <p className="mt-2 text-[#666e75 text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+                    <button
+                        type="submit"
+                        className="mt-3 text-white bg-[#6449ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    >
+                        {loading ? 'Sharing...' : 'Share Image'}
+                    </button>
 
                 </div>
             </form>
